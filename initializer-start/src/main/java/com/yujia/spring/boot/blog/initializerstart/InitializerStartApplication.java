04bd1b9ffcd84ca84e8858566287ced884fab375
Yujia Lian <yujia.lian001@gmail.com>
2017-12-19 13:16:40 -0500

    Initial commit

---
+++ initializer-start/src/main/java/com/yujia/spring/boot/blog/initializerstart/InitializerStartApplication.java
@@ -0,0 +1,12 @@
+package com.yujia.spring.boot.blog.initializerstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerStartApplication.class, args);
+	}
+}
